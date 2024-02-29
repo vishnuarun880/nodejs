35fc07d923fd05c8594bdf10ce39bb14c7a3d810
vishnuarun880 <145054736+vishnuarun880@users.noreply.github.com>
2024-02-29 14:24:34 +0530

    Update index.js

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const port = process.env.PORT || 3000;
 
 const server = http.createServer((req, res) => {
   res.statusCode = 200;
-  const msg = 'hello my new push! \n'
+  const msg = 'hello my new push! nnn\n'
   res.end(msg);
 });
 
